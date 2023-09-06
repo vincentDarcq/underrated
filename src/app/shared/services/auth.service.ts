4c1a862fe48faf0f7e31416f72bd723071ca49d9
Vincent Darcq <vincentdarcq@MacBook-Air-de-Vincent.local>
2023-09-06 19:57:58 +0200

    angular 16

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,11 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService implements CanActivate{
+export class AuthService {
 
   token: string;
   valid: boolean;
